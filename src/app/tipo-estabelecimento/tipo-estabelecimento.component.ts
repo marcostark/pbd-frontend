a6fbf86272472dc7e4e2b8dd9230b5eb33d145ae
Stark <mrcs.stark@gmail.com>
2019-05-25 15:40:09 -0300

    Consumindo endpoint de tipos de estabelecimentos e listando

--- src/app/tipo-estabelecimento/tipo-estabelecimento.component.ts
+++ src/app/tipo-estabelecimento/tipo-estabelecimento.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TipoEstabelecimentoService } from './tipo-estabelecimento.service';
+import { TipoEstabelecimentoModel } from 'app/dashboard/model/tipo-estabelecimento.model';
 
 @Component({
   selector: 'app-tipo-estabelecimento',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tipo-estabelecimento.component.scss']
 })
 export class TipoEstabelecimentoComponent implements OnInit {
+  
+  tiposEstabelecimentos: TipoEstabelecimentoModel[];
 
-  constructor() { }
+  constructor(
+    private service: TipoEstabelecimentoService
+  ) { }
 
   ngOnInit() {
+    console.log("hello")
+    this.getData()
+  }
+
+  getData(){
+    this.service.getTipos().subscribe(
+      tiposEstabelecimentos => {
+        this.tiposEstabelecimentos = tiposEstabelecimentos;  
+        console.log(this.tiposEstabelecimentos)             
+      },      
+      erro => console.error(erro)
+    )
   }
 
 }
--- src/app/tipo-estabelecimento/tipo-estabelecimento.service.ts
+++ src/app/tipo-estabelecimento/tipo-estabelecimento.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { TipoEstabelecimentoModel } from 'app/dashboard/model/tipo-estabelecimento.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TipoEstabelecimentoService {
 
-  constructor() { }
+  constructor(
+    @Inject('TIPO_ESTABELECIMENTO_ENDPOINT') private tipoEndpoint: string,
+    private htttpClient: HttpClient
+  ) { }
+
+  getTipos(): Observable<TipoEstabelecimentoModel[]> {
+    return this.htttpClient.get<TipoEstabelecimentoModel[]>(this.tipoEndpoint);
+  }
+
+  getTipo(id: number): Observable<TipoEstabelecimentoModel> {
+    return this.htttpClient.get<TipoEstabelecimentoModel>(this.tipoEndpoint + id + '/');
+  }
+
 }
