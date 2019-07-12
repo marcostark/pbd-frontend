7cef08679912ef2507b0be790afb9c9b11a718bc
stark <mrcs.stark@gmail.com>
2019-07-12 00:54:12 -0300

    Tipo de produto

--- src/app/tipo-produto/tipo-produto.module.ts
+++ src/app/tipo-produto/tipo-produto.module.ts
@@ -2,11 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TipoProdutoRoutingModule } from './tipo-produto-routing.module';
 import { TipoProdutoComponent } from './tipo-produto.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  MatButtonModule,
+  MatInputModule,
+  MatRippleModule,
+  MatFormFieldModule,
+  MatTooltipModule,
+  MatDialogModule,
+  MatSelectModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     TipoProdutoRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatRippleModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatTooltipModule,
+    MatSelectModule
   ],
   declarations: [
     TipoProdutoComponent,
